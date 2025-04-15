669468f015a722a940becc303534614307f5a7c3
marutina98 <martinamineo98@gmail.com>
2025-04-15 14:44:20 +0200

    Feat: configuring PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,38 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import PrimeVue from 'primevue/config';
+
+// Vue
 
 import App from './App.vue';
 import router from './router';
 
+// PrimeVue
+
+import PrimeVue from 'primevue/config';
+
+// PrimeVue Themes
+
+import Nora from '@primeuix/themes/nora';
+
+// import Aura from '@primeuix/themes/aura';
+// import Lara from '@primeuix/themes/lara';
+// import Material from '@primeuix/themes/material';
+
+const primeVueSettings = {
+  theme: {
+    preset: Nora,
+    options: {
+      prefix: '.p',
+      darkModeSelector: '.d',
+      cssLayer: false,
+    }
+  }
+}
+
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(PrimeVue);
+app.use(PrimeVue, primeVueSettings);
 app.use(router);
 
 app.mount('#app');
