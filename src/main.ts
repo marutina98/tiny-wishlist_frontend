7aa129492a4befcb3f59a9a519ead2d9c338a724
marutina98 <martinamineo98@gmail.com>
2025-04-15 14:53:10 +0200

    Feat: removed PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import '@/assets/main.css';
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
@@ -6,33 +8,9 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-// PrimeVue
-
-import PrimeVue from 'primevue/config';
-
-// PrimeVue Themes
-
-import Nora from '@primeuix/themes/nora';
-
-// import Aura from '@primeuix/themes/aura';
-// import Lara from '@primeuix/themes/lara';
-// import Material from '@primeuix/themes/material';
-
-const primeVueSettings = {
-  theme: {
-    preset: Nora,
-    options: {
-      prefix: '.p',
-      darkModeSelector: '.d',
-      cssLayer: false,
-    }
-  }
-}
-
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(PrimeVue, primeVueSettings);
 app.use(router);
 
 app.mount('#app');
