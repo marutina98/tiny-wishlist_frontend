4ada2bcf1ae3b5ae0d00b251fddb7c6325bc9db6
marutina98 <martinamineo98@gmail.com>
2025-04-16 21:24:29 +0200

    Feat: moved isAuthenticated in DefaultTemplate for NavigationMenu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-
-  import { computed, inject } from 'vue';
-  import type IAuth from './interfaces/auth.interface';
-
-  const auth = inject('auth') as IAuth;
-
-  const isAuthenticated = computed(() => auth.isAuthenticated());
-
 </script>
 
 <template>
   <UApp>
-    <RouterView :isAuthenticated />
+    <RouterView />
   </UApp>
 </template>
 
--- src/templates/DefaultTemplate.vue
+++ src/templates/DefaultTemplate.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+
+  import { computed, inject } from 'vue';
+  import type IAuth from '@/interfaces/auth.interface';
+
   import NavigationMenu from '@/components/NavigationMenu.vue';
+
+  const auth = inject('auth') as IAuth;
+  const isAuthenticated = computed(() => auth.isAuthenticated());
+
 </script>
 
 <template>
 
-  <NavigationMenu />
+  <NavigationMenu :isAuthenticated />
 
   <main>
     <slot name="main"></slot>
