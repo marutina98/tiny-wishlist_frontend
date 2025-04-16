901908e8e1d8c6691021bd9d57d2b086aa601cf2
marutina98 <martinamineo98@gmail.com>
2025-04-16 21:47:20 +0200

    Feat: started working on NavigationMenu

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -3,16 +3,81 @@
   import type { Ref } from 'vue';
   import type { NavigationMenuItem } from '@nuxt/ui';
   
-  import { ref } from 'vue';
+  import { ref, computed, watch, onBeforeMount } from 'vue';
+  
+  const props = defineProps({
+    isAuthenticated: Boolean
+  });
 
-  const items: Ref<NavigationMenuItem[]> = ref<NavigationMenuItem[]>([
-    
-  ]);
+  const isAuthenticated = computed(() => props.isAuthenticated);
+
+  const items: Ref<NavigationMenuItem[]> = ref([]);
+
+  // Watch isAuthenticated status
+
+  watch(isAuthenticated, (status: boolean) => {
+    setItems(status);
+  });
+
+  const setItems = (status: boolean) => {
+
+    const navigationMenuItems: NavigationMenuItem[] = [
+      {
+        label: 'Homepage',
+        icon: 'i-system-uicons:home',
+        to: '/',
+      }
+    ];
+
+    // @todo: if guest show login/register
+    // otherwise show dashboard and logout
+
+    if (status) {
+
+      navigationMenuItems.push(...[
+        {
+          label: 'Dashboard',
+          icon: 'i-system-uicons:settings',
+        },
+        {
+          label: 'Logout',
+          icon: 'i-system-uicons:close',
+        }
+      ]);
+
+    } else {
+
+      navigationMenuItems.push(...[
+        {
+          label: 'Authenticate',
+          icon: 'i-system-uicons:user-male',
+          children: [
+            {
+              label: 'Sign In'
+            },
+            {
+              label: 'Sign Up'
+            }
+          ]
+        }
+      ])
+
+    }
+
+    items.value = navigationMenuItems;
+
+  }
+
+  onBeforeMount(() => {
+    setItems(isAuthenticated.value);
+  });
 
 </script>
 
 <template>
-  <UNavigationMenu :items />
+  <UContainer>
+    <UNavigationMenu :items />
+  </UContainer>
 </template>
 
 <script scoped></script>
