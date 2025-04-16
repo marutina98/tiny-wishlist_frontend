dac2cdbe9429ca0fbe97904ddb3904f818152fb0
marutina98 <martinamineo98@gmail.com>
2025-04-16 20:34:41 +0200

    Feat: working SApi

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,41 +1,59 @@
+
+// @todo: move interfaces in their own file
+
 interface IRequest {
   apiURL: string,
   method: string,
   token?: string,
-  body?: string,
+  body?: object,
+}
+
+interface IRequestRegister {
+  email: string,
+  username: string,
+  password: string,
+}
+
+interface IRequestLogin {
+  username: string,
+  password: string,
 }
 
 class SApi {
 
-  private baseURL: string = 'http://localhost:3000/';
+  private baseURL: string = 'http://localhost:3000';
 
-  public async register() {
-
+  public async register(body: IRequestRegister) {
+    return await this.getRequest({
+      apiURL: `${this.baseURL}/auth/register`,
+      method: 'POST',
+      body
+    });
   }
 
-  public async login() {
-
+  public async login(body: IRequestLogin) {
+    return await this.getRequest({
+      apiURL: `${this.baseURL}/auth/login`,
+      method: 'POST',
+      body
+    });
   }
 
-  public getRequest({ request }: { request: IRequest }) {
-
-    const headers = this.getHeaders(request);
-
+  public getRequest(request: IRequest) {
     return fetch(request.apiURL, {
       mode: 'cors',
       method: request.method,
-      headers
+      headers: this.getHeaders(request),
     });
-
   }
 
   public getHeaders({ method, token }: { method: string, token?: string }) {
 
+    const contentTypeJSON = ['POST', 'PUT'];
+
     const headers = [
       ['Access-Control-Allow-Headers', '*'],
     ];
-
-    const contentTypeJSON = ['POST', 'PUT'];
 
     if (contentTypeJSON.includes(method)) {
       headers.push(['Content-Type', 'application/json']);
