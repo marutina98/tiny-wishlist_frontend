47fe3a96a2c334efa0cc15b4ef78000c3f9612e8
marutina98 <martinamineo98@gmail.com>
2025-04-17 15:57:56 +0200

    Fix: added missing semicolons

--- src/guards/is-guest.guard.ts
+++ src/guards/is-guest.guard.ts
@@ -4,14 +4,14 @@ import type IAuth from '@/interfaces/auth.interface';
 import { inject } from 'vue';
 
 export function GIsGuest (to: RouteLocationNormalized, from: RouteLocationNormalized, next: Function) {
-  
+
   const auth = inject('auth') as IAuth;
   const isGuest = auth.isGuest();
 
   if (isGuest) {
     next();
   } else {
-    next({ path: '/' })
+    next({ path: '/' });
   }
 
 }
--- src/guards/is-user.guard.ts
+++ src/guards/is-user.guard.ts
@@ -4,14 +4,14 @@ import type IAuth from '@/interfaces/auth.interface';
 import { inject } from 'vue';
 
 export function GIsUser (to: RouteLocationNormalized, from: RouteLocationNormalized, next: Function) {
-  
+
   const auth = inject('auth') as IAuth;
   const isAuthenticated = auth.isAuthenticated();
 
   if (isAuthenticated) {
     next();
   } else {
-    next({ path: '/' })
+    next({ path: '/' });
   }
 
 }
