a9dad7fb133fb8cad6c695c5de9f3bd7621b53ec
marutina98 <martinamineo98@gmail.com>
2025-04-17 18:19:31 +0200

    Feat: working on validation with valibot

--- src/templates/AlternativeTemplate.vue
+++ src/templates/AlternativeTemplate.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <main>
+  <main class="min-w-screen min-h-screen flex items-center justify-center">
     <slot name="main"></slot>
   </main>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,11 +6,21 @@
   import { reactive } from 'vue';
   
   const schema = v.object({
+
     username: v.pipe(
       v.string(),
       v.nonEmpty('Please enter your username.'),
-      v.regex(/^[A-Za-z0-9]{8,}$/, 'The username can only contain letters and numbers'),
+      v.regex(/^[A-Za-z0-9]*$/, 'Your username can only contain letters and numbers.'),
+      v.minLength(8, 'Your username must have 8 characters or more.'),
+    ),
+
+    password: v.pipe(
+      v.string(),
+      v.nonEmpty('Please enter your password.'),
+      v.regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/, 'Your password must contain at least a lowercase character, an uppercase character, a number and a special character.'),
+      v.minLength(8, 'Your password must have 8 characters or more.'),
     )
+
   });
 
   const state = reactive({
@@ -27,7 +37,7 @@
 <template>
   <AlternativeTemplate>
     <template v-slot:main>
-      <UForm :schema :state @submit.prevent="onSubmit">
+      <UForm class="space-y-4" :schema :state @submit.prevent="onSubmit">
         <UFormField label="Username" name="username">
           <UInput v-model="state.username" />
         </UFormField>
