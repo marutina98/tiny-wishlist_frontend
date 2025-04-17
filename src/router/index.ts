8165b59642083b3f7cbe95aaaa4caa37fd7f14e4
marutina98 <martinamineo98@gmail.com>
2025-04-17 15:40:04 +0200

    Feat: added ErrorRedirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import HomeView from '@/views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import ErrorRedirect from '@/components/ErrorRedirect.vue';
+
+import { GIsUser } from '@/guards/is-user.guard';
+import { GIsGuest } from '@/guards/is-guest.guard';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
 
     {
+      path: '/:pathMatch(.*)*',
+      component: ErrorRedirect
+    },
+
+    {
       path: '/',
       component: HomeView,
     },
+
+    {
+      path: '/login',
+      component: LoginView,
+    }
 
   ],
 });
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import AlternativeTemplate from '../templates/AlternativeTemplate.vue';
+</script>
+
+<template>
+  <AlternativeTemplate>
+    <template v-slot:main>
+      Login
+    </template>
+  </AlternativeTemplate>
+</template>
+
+<style scoped>
+</style>
