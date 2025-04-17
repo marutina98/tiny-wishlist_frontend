fcdde5b9fbb19b65a42b2975c3cffceff6fa5cee
marutina98 <martinamineo98@gmail.com>
2025-04-17 18:06:31 +0200

    Feat: working on LoginView Form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
+
+  import * as v from 'valibot';
+
   import AlternativeTemplate from '../templates/AlternativeTemplate.vue';
+  import { reactive } from 'vue';
+  
+  const schema = v.object({
+    username: v.pipe(
+      v.string(),
+      v.nonEmpty('Please enter your username.'),
+      v.regex(/^[A-Za-z0-9]{8,}$/, 'The username can only contain letters and numbers'),
+    )
+  });
+
+  const state = reactive({
+    username: '',
+    password: '',
+  });
+
+  const onSubmit = async () => {
+    console.log(state);
+  }
+
 </script>
 
 <template>
   <AlternativeTemplate>
     <template v-slot:main>
-      Login
+      <UForm :schema :state @submit.prevent="onSubmit">
+        <UFormField label="Username" name="username">
+          <UInput v-model="state.username" />
+        </UFormField>
+
+        <UFormField label="Password" name="password">
+          <UInput v-model="state.password" type="password" />
+        </UFormField>
+
+        <UButton type="submit">
+          Submit
+        </UButton>
+      </UForm>
     </template>
   </AlternativeTemplate>
 </template>
