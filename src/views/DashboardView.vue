50a5721baf125c396082a693898bd04ee6b34c37
marutina98 <martinamineo98@gmail.com>
2025-04-18 16:47:57 +0200

    Feat: created DashboardView

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import DefaultTemplate from '../templates/DefaultTemplate.vue';
+</script>
+
+<template>
+  <DefaultTemplate>
+    <template v-slot:main>
+      Dashboard
+    </template>
+  </DefaultTemplate>
+</template>
+
+<style scoped>
+</style>
