eeacc0013e3b3db78fbf691cf0cf07fbb6253cfa
marutina98 <martinamineo98@gmail.com>
2025-04-22 21:39:50 +0200

    Feat: started working in Dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,14 +1,61 @@
 <script setup lang="ts">
+
   import DefaultTemplate from '../templates/DefaultTemplate.vue';
+
+  import { ref } from 'vue';
+
+  import type { Ref } from 'vue';
+  import type { NavigationMenuItem } from '@nuxt/ui';
+
+  const items: Ref<NavigationMenuItem[]> = ref([
+    {
+      label: 'Profile',
+      defaultOpen: true,
+      children: [
+        {
+          label: 'View',
+        },
+        {
+          label: 'Edit',
+        }
+      ]
+    },
+    {
+      label: 'Wishlists',
+      defaultOpen: true,
+      children: [
+        {
+          label: 'View',
+        },
+        {
+          label: 'Create',
+        }
+      ]
+    }
+  ]);
+
+  const nav = {
+    class: 'left navigation-menu',
+    orientation: 'vertical' as 'vertical' | 'horizontal',
+    items: items.value,
+  }
+
 </script>
 
 <template>
   <DefaultTemplate>
     <template v-slot:main>
-      Dashboard
+      <UNavigationMenu v-bind="nav" />
     </template>
   </DefaultTemplate>
 </template>
 
 <style scoped>
+
+  @reference 'tailwindcss';
+
+  .left.navigation-menu {
+    @apply w-48;
+  }
+
 </style>
