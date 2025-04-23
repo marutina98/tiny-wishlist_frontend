3895d641a96b3dfdbdc7068fb411f4a8154650c3
marutina98 <martinamineo98@gmail.com>
2025-04-23 14:41:24 +0200

    Feat: working in Wishlists

--- src/components/Wishlists.vue
+++ src/components/Wishlists.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 
-  import { computed, onBeforeMount, ref } from 'vue';
+  import { computed, onBeforeMount, ref, type Ref } from 'vue';
 
+  import type { TreeItem } from '@nuxt/ui';
   import type IUser from '@/interfaces/user.interface';
 
   const props = defineProps({
@@ -15,11 +16,84 @@
 
   // Lists as File Tree
 
-  const items = ref([]);
+  const items: Ref<TreeItem[]> = ref([]);
 
   onBeforeMount(() => {
 
+    // Get wishlists with their statuses
+
+    const publicLists = [];
+    const privateLists = [];
+    
+    const archivedPublicLists: TreeItem[] = [];
+    const archivedPrivateLists: TreeItem[] = [];
+
+    if (lists.value) {
+
+      for (let list of lists.value) {
+
+        const listObj = {
+          label: list.title
+        }
+
+        if (list.private) {
+
+          if (list.archived) {
+            archivedPrivateLists.push(listObj);
+          } else {
+            privateLists.push(listObj);
+          }
+
+        } else {
+
+          if (list.archived) {
+            archivedPublicLists.push(listObj);
+          } else {
+            publicLists.push(listObj);
+          }
+
+        }
+
+      }
+      
+    }
+    
+    const archivedLists = [
+      {
+        label: 'Public Lists',
+        children: archivedPublicLists,
+      },
+      {
+        label: 'Private Lists',
+        children: archivedPrivateLists,
+      }
+    ];
+
     // Set items
+
+    const _items = [
+      {
+        label: 'Wishlists',
+        icon: 'i-system-uicons:gift',
+        defaultExpandend: true,
+        children: [
+          {
+            label: 'Private Wishlists',
+            children: privateLists,
+          },
+          {
+            label: 'Public Wishlists',
+            children: publicLists,
+          },
+          {
+            label: 'Archived Wishlists',
+            children: archivedLists,
+          }
+        ]
+      }
+    ];
+
+    items.value = _items;
 
   });
 
@@ -27,12 +101,27 @@
 
 <template>
 
-  {{ lists }}
-
-  Wishlists
+  <div class="wishlists-wrapper">
+    <div class="tree">
+      <UTree :items />
+    </div>
+    <div class="content">
+      Content
+    </div>
+  </div>
   
 </template>
 
 <style scoped>
+
   @reference 'tailwindcss';
+
+  .wishlists-wrapper {
+    @apply flex flex-row gap-2;
+  }
+
+  .content {
+    @apply w-1/3;
+  }
+
 </style>
