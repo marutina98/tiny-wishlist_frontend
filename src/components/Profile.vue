babb727ef7e8a66ede0cf23fe8802aa78085021a
marutina98 <martinamineo98@gmail.com>
2025-04-23 15:28:55 +0200

    Feat: started working in Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -15,12 +15,28 @@
 
 <template>
 
-  Profile
+  <div class="profile-preview">
 
-  {{ user }}
-  
+    <div class="profile-email">
+      <span class="profile-label">Email</span>
+      <span class="profile-content">{{ user.email }}</span>
+    </div>
+
+    <div class="profile-username">
+      <span class="profile-label">Username</span>
+      <span class="profile-content">{{ user.username }}</span>
+    </div>
+    
+  </div>
+
 </template>
 
 <style scoped>
+
   @reference 'tailwindcss';
+
+  .profile-preview {
+    
+  }
+
 </style>
