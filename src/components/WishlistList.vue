eed37cea47f934a7fe0d5bdeb22762dfe06ffeee
marutina98 <martinamineo98@gmail.com>
2025-04-24 14:48:08 +0200

    Feat: working in WishlistList

--- src/components/WishlistList.vue
+++ src/components/WishlistList.vue
@@ -14,5 +14,32 @@ import { computed } from 'vue';
 </script>
 
 <template>
-  {{ list }}
+
+  <div class="list">
+
+    <div class="settings">
+      <UButton icon="i-system-uicons:plus-circle" label="Create Group" />
+      <UButton icon="i-system-uicons:plus-circle" label="Add Item" />
+    </div>
+  
+    <div :class="{ groups: true, 'no-groups': list.groups.length <= 0 }">
+      There are no groups present in this list.
+    </div>
+
+  </div>
+
 </template>
+
+<style scoped>
+
+  @reference 'tailwindcss';
+
+  .settings {
+    @apply flex gap-2 justify-end;
+  }
+
+  .groups {
+    @apply text-center text-sm;
+  }
+
+</style>
