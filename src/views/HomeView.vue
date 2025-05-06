e7f96ce321e4f7e34d66bf507f13e84211e4bcce
marutina98 <martinamineo98@gmail.com>
2025-05-06 22:53:56 +0200

    Feat: started working on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
+  
+  import { inject, computed } from 'vue';
+
   import DefaultTemplate from '../templates/DefaultTemplate.vue';
+  
+  import type IAuth from '@/interfaces/auth.interface';
+
+  const auth = inject('auth') as IAuth;
+  const isLogged = computed(() => auth.isAuthenticated());
+
 </script>
 
 <template>
   <DefaultTemplate>
     <template v-slot:main>
-      Homepage
+      
+      <div class="wrapper">
+
+        <template v-if="isLogged">
+          Logged
+        </template>
+
+        <template v-else>
+          Guest
+        </template>
+
+      </div>
+
     </template>
   </DefaultTemplate>
 </template>
 
 <style scoped>
+
+  @reference 'tailwindcss';
+
+  .wrapper {
+    --navigation-menu-height: 81px; /* magic number */
+    @apply flex items-center justify-center;
+    min-height: calc(100vh - var(--navigation-menu-height));
+  }
+
 </style>
