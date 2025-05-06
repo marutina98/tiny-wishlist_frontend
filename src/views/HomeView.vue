1265a0e388bf71373f03998620d79d53c0150b0a
marutina98 <martinamineo98@gmail.com>
2025-05-06 23:58:09 +0200

    Feat: homepage done (for now)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,13 +17,26 @@
       
       <div class="wrapper">
 
-        <template v-if="isLogged">
-          Logged
-        </template>
+        <div class="greeting">
+          Hello!
+        </div>
 
-        <template v-else>
-          Guest
-        </template>
+        <div class="text">
+          Create your own shareable wishlist.
+        </div>
+
+        <div class="buttons">
+
+          <template v-if="isLogged">
+            <UButton size="xl" to="/dashboard" label="Dashboard" />
+          </template>
+
+          <template v-else>
+            <UButton size="xl" to="/login" label="Login" />
+            <UButton size="xl" to="/register" label="Register" />
+          </template>
+
+        </div>
 
       </div>
 
@@ -37,8 +50,20 @@
 
   .wrapper {
     --navigation-menu-height: 81px; /* magic number */
-    @apply flex items-center justify-center;
+    @apply flex flex-col gap-2 items-center justify-center;
     min-height: calc(100vh - var(--navigation-menu-height));
+  }
+
+  .greeting {
+    @apply text-8xl;
+  }
+
+  .text {
+    @apply text-3xl;
+  }
+
+  .buttons {
+    @apply flex flex-row gap-4 mt-2;
   }
 
 </style>
